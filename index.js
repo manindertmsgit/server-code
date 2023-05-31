4dbe95bb4d587116021a38af2c4ada57059db202
minnie <maninderjeet28@gmail.com>
2023-06-01 02:25:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+app.listen(8080, function () {
+  console.log('Server listening at 8080');
+});
