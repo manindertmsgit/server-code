2b877c59b8443f9c9d61c9c8a779f5ad95872554
minnie <maninderjeet28@gmail.com>
2023-06-02 20:53:03 +0530

    first hit api endpoint

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/', function (req, res) {
+//     res.json({
+//       data: ' First Api Endpoint!' 
+//     })
+//   });
+// app.listen(8080, function () {
+//   console.log('Server listening at 8080');
+// });
+
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+  res.json({
+    data: 'First API Endpoint!'
   });
-app.listen(8080, function () {
-  console.log('Server listening at 8080');
 });
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
+
+
+
